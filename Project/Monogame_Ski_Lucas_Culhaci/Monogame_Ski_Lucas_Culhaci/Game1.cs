2e50923e1a2667924c14876c2ff28dffe3ed8ba2
lucasCulhaci <40703191+lucasCulhaci@users.noreply.github.com>
2025-12-21 22:56:55 +0100

    Basic Configuration

--- Project/Monogame_Ski_Lucas_Culhaci/Monogame_Ski_Lucas_Culhaci/Game1.cs
+++ Project/Monogame_Ski_Lucas_Culhaci/Monogame_Ski_Lucas_Culhaci/Game1.cs
@@ -1,24 +1,60 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Monogame_Ski_Lucas_Culhaci.Object;
+using Monogame_Ski_Lucas_Culhaci.States.Base;
+using System.Collections.Generic;
 
 namespace Monogame_Ski_Lucas_Culhaci
 {
     public class Game1 : Game
     {
+
+        internal const int PLAYER_STEP = 10;
+        internal const int BACKGROUND_STEP = 2;
+        internal const int OBSTACLE_STEP = 2;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        internal SpriteFont _font;
+
+        internal int _numberOfRemainingLives;
+
+        internal PlayerSprite Player;
+
+        internal Texture2D _background;
+        internal Vector2 _backgroundPosition;
+
+        internal double _elapsedTimeSinceLastBoulderInMs;
+        internal double _elapsedTimeSinceLastTreeTrunkInMs;
+        internal double _elapsedTimeSinceLastSnowmanInMs;
+
+        internal Texture2D _boulder;
+        internal Texture2D _treeTrunk;
+        internal Texture2D _snowman;
+
+        internal List<Vector2> _boulderPositions;
+        internal List<Vector2> _treeTrunkPositions;
+        internal List<Vector2> _snowmanPositions;
+
+        private State _currentState;
+
+
+        #region Constructor
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
+        #endregion
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferHeight = 1280;
+            _graphics.PreferredBackBufferWidth = 720;
+            _graphics.ApplyChanges();
+
 
             base.Initialize();
         }
---
+++ Project/Monogame_Ski_Lucas_Culhaci/Monogame_Ski_Lucas_Culhaci/States/Base/State.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Monogame_Ski_Lucas_Culhaci.States.Base
+{
+    public abstract class State(Game1 context)
+    {
+
+        protected Game1 Context { get; } = context;
+        
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(GameTime gameTime);
+
+    }
+}
